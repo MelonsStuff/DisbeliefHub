0e05b0b96db3346854c238123ffeb8bcf0a0818b
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-07-09 15:12:21 -0500

    Create ForsakenAnimations.lua

---
+++ Scripts/Animations/ForsakenAnimations.lua
@@ -0,0 +1,12 @@
+local Forsaken = {
+{Name = "Shucks", ID = 74238051754912},
+{Name = "Silly Billy", ID = 107464355830477},
+{Name = "Subterfuge", ID = 87482480949358},
+{Name = "Richard Watterson (the hero)", ID = 74012585744751},
+{Name = "Tick Tock", ID = 74187447524398},
+{Name = "Ain't No Lovin My Man", ID = 113329473982836},
+{Name = "I Miss The Quiet", ID = 100986631322204},
+{Name = "Eggman Dance", ID = 93910549909381},
+}
+
+return Forsaken
