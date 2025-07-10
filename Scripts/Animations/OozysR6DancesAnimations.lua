eb0c7594bcb7908c4a4b1c8501a00ca4f0749a7b
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-07-10 17:19:51 -0500

    Create OozysR6DancesAnimations.lua

---
+++ Scripts/Animations/OozysR6DancesAnimations.lua
@@ -0,0 +1,24 @@
+local OozysR6Dances = {
+{Name = "California", ID = 113427251195560},
+{Name = "Sugar", ID = 122916078173123},
+{Name = "Boston BreakDance", ID = 78570455464256},
+{Name = "Breakdown", ID = 71749175995780},
+{Name = "Spin", ID = 138612241499030},
+{Name = "Sleepy", ID = 87177032312242},
+{Name = "Pizza Tower", ID = 74706771494292},
+{Name = "CatDown", ID = 74750058433101},
+{Name = "Mewing", ID = 121216021424341},
+{Name = "Gyrating", ID = 135053448465416},
+{Name = "Arona", ID = 113953995772094},
+{Name = "Demoman", ID = 131823883329199},
+{Name = "Nizmoo", ID = 102022929842437},
+{Name = "Car Shearer", ID = 82286209518466},
+{Name = "Hip Shop", ID = 124519546671156},
+{Name = "Dance Potion", ID = 122874098195375},
+{Name = "Hallway", ID = 119606589130611},
+{Name = "Surfin' Bird", ID = 75703012837662},
+{Name = "Slick", ID = 125987679042998},
+{Name = "Silly", ID = 98897446099638},
+}
+
+return OozysR6Dances
